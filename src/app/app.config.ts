9e8c7b27a162bcd792182e2ee81617e6ab5b6108
KrolikCthulhu <darya.firzina@mail.ru>
2024-04-18 15:52:31 +0500

    add env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideStore } from '@ngrx/store';
+import { StoreModule, provideStore } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideStore()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideStore(),
+    importProvidersFrom(
+      // EffectsModule.forRoot([UserEffects, TaskEffects]]),
+      // StoreModule.forRoot(appReducers),
+      StoreDevtoolsModule.instrument({
+        maxAge: 25,
+        logOnly: environment.production,
+      })
+    ),
+  ],
 };
